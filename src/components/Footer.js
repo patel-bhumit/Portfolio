253f0fc23520afe6d5fcf45576d65459715fe323
patel-bhumit <bhumit667@gmail.com>
2022-06-23 21:54:21 +0530

    V1.0.2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
         <footer className="text-center text-white bg-dark-theme" style={{backgroundcolor: "#21081a"}}>
   <div className="container-fluid"></div>
   <div className="text-center p-3" style={{backgroundcolor: "rgba(0, 0, 0, 0.2);"}}>
-    © 2020 Copyright: Bhumit Patel </div>
+    © 2022 Copyright: Bhumit Patel </div>
 </footer>
     </div>
     );
